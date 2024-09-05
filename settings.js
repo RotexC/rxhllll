724b02cbf241674e68154690060518074ccbe81a
jaylovesbundafr <Kila01190@gmail.com>
2024-09-05 12:35:54 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 global.prefa = ['','!','.',',','🐤','🗿']
 
-global.owner = ['2349018468543']
+global.owner = ['2349034571442']
 global.botname = 'Rxhl Bot'
 global.baileys1 = require('@whiskeysockets/baileys') 
 global.sticker1 = "Rxhl Official"

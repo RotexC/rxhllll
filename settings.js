4cc6c5d3d9f3ca4ff063d91a9a557da669788ba8
RotexC <155334810+RotexC@users.noreply.github.com>
2024-09-12 23:15:41 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 global.prefa = ['','!','.',',','🐤','🗿']
 
-global.owner = ['+2349114224240']
+global.owner = ['2348137420869']
 global.botname = 'Rxhl Bot'
 global.baileys1 = require('@whiskeysockets/baileys') 
 global.sticker1 = "Rxhl Official"
